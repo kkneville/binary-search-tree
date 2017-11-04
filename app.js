f83d87d4c4bfc0c67c6f656f8264c0cafc31fc1e
kkneville <mskatherinekrueger@gmail.com>
2017-11-04 10:48:31 -0500

    adding brain and tree

---
+++ app.js
@@ -0,0 +1,96 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:    
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('views/index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/tree') {
+        fs.readFile('views/tree.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/tree.js') {
+        fs.readFile('js/tree.js', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/javascript'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/tree.csv') {
+        fs.readFile('csv/tree.csv', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'application/octet-stream'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/brain') {
+        fs.readFile('views/brain.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/brain.js') {
+        fs.readFile('js/brain.js', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/javascript'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/flare.csv') {
+        fs.readFile('csv/flare.csv', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'application/octet-stream'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/numbers.csv') {
+        fs.readFile('csv/numbers.csv', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'application/octet-stream'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/bst') {
+        fs.readFile('views/bst.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/bst.css') {
+        fs.readFile('css/bst.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/bst.js') {
+        fs.readFile('js/bst.js', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/javascript'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+ 
+    else {
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
---
+++ js/brain.js
@@ -0,0 +1,81 @@
+// $(document).ready(function(){
+// 	$("#seed").hide()
+// });
+
+
+
+function generateSeed() {		
+  seed = newSeed()
+  console.log(seed)
+  $("#seed").show()
+  document.getElementById("seed").innerHTML = seed
+}
+
+var newSeed = function(){
+      var b = []
+      for (var i = 0; i < 200; i++){
+        b.push(Math.floor(Math.random()*100))
+      }
+      return b
+	}
+
+function Leaf(val, addr, prev=null) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+  this.addr = addr;
+  this.prev = prev;
+}
+
+function BinarySearchTree() {
+  this.root = null;
+}
+
+
+var grow = function(list){
+  var bst = new BinarySearchTree();
+  for (var i = 0; i < list.length; i++) {
+    bst.add(list[i]);
+  }
+  return bst;
+}
+
+
+BinarySearchTree.prototype.add = function(val) {
+  var root = this.root;
+  var runner = root;
+  if (!root) {
+    this.root = new Leaf(val);
+    return;
+  }
+
+  var y = 1
+
+  while (runner) {
+
+    if (val < runner.value) {
+      if (!runner.left) {
+        runner.left = new Leaf(val, runner);
+        break;
+      } 
+      else {
+        runner = runner.left;
+      }
+    } 
+
+    else if (val > runner.value ) {
+      if (!runner.right) {
+        runner.right = new Leaf(val, runner);
+        break;
+      } 
+      else {
+        runner = runner.right;
+      }
+	}
+
+	else {
+		console.log('value already in tree');
+    	return;
+	}
+  }
+}
